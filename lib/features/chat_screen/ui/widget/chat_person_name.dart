97b0589395afcaf4c7111aa5b5990c7e0d57c398
antoneosphilip <109253442+antoneosphilip@users.noreply.github.com>
2024-07-24 19:06:52 -0700

    add friend

---
+++ lib/features/chat_screen/ui/widget/chat_person_name.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatPersonName extends StatelessWidget {
+  const ChatPersonName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
